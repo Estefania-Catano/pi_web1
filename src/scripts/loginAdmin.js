9eaa85192b6ab53e71c0de44573e6581ddbd1bae
yeisson <yeionossa3010@gmail.com>
2025-09-18 20:18:48 -0500

    feature: Attempted to add login authentication

---
+++ src/scripts/loginAdmin.js
@@ -0,0 +1,10 @@
+const validUser = {
+    'admin':'123456',
+    'email':'admin@mail.com',
+    'demo':'demo'
+}
+
+
+const loginForm = document.getElementById('loginForm')
+const passwordInput = document.getElementById('password')
+const emailInput = document.getElementById('email')
